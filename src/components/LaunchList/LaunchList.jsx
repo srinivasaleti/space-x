4712c7ab43e41e7cb3117c6d63d254a30cc0d14e
Srinivasarao Aleti <srinivas.aleti03@gmail.com>
2020-05-27 20:43:41 +0530

    Display original luanches

--- src/components/LaunchList/LaunchList.jsx
+++ src/components/LaunchList/LaunchList.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
 import { Launch } from '../Launch/Launch';
 import './styles.css';
+import axios from 'axios';
 
 export class LaunchList extends React.Component {
+
+  state = {
+    launches: []
+  }
+
+  componentDidMount = () => {
+    this.getLaunches()
+  }
+
+  getLaunches = () => {
+    axios.get('https://api.spacexdata.com/v3/launches').then((response) => {
+      this.setState(
+        { launches: response.data }
+      )
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  launchList = () => {
+    const launchListComponents = this.state.launches.map((launch,index) => {
+      const image = launch.links.flickr_images.length === 0 ?
+        'https://upload.wikimedia.org/wikipedia/commons/9/9a/Soyuz_TMA-9_launch.jpg' : launch.links.flickr_images[0];
+      return <Launch
+        key={"launch_"+index}
+        banner={image}
+        title={launch.mission_name}
+        launchDate={launch.launch_date_local}
+        description={launch.details} />
+    })
+
+    return launchListComponents;
+  }
+
   render() {
     return (
       <div className="launch-list">
-        <Launch
-          banner="https://farm8.staticflickr.com/7615/16670240949_8d43db0e36_o.jpg"
-          title="FalconSat"
-          launchDate="2006-03-25T10:30:00+12:00"
-          description="Some Description"
-        />
-
-        <Launch
-          banner="https://farm8.staticflickr.com/7615/16670240949_8d43db0e36_o.jpg"
-          title="Trailblazer"
-          launchDate="2008-08-02T15:34:00+12:00"
-          description="Some Description"
-        />
-
-        <Launch
-          banner="https://farm8.staticflickr.com/7615/16670240949_8d43db0e36_o.jpg"
-          title="Trailblazer"
-          launchDate="2008-08-02T15:34:00+12:00"
-          description="Some Description"
-        />
+          {this.launchList()}
       </div>
     )
   }
