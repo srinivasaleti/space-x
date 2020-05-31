c890d6651c0081908fec73d8574762b3b838df0b
Srinivasarao Aleti <srinivas.aleti03@gmail.com>
2020-05-31 20:26:35 +0530

    Display launch based on route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header } from './components/Header/Header';
 import './styles.css'
 import { LaunchList } from './components/LaunchList/LaunchList';
-import { LaunchView } from './components/LaunchView/LaunchView';
+import LaunchView from './components/LaunchView/LaunchView';
 import {
   BrowserRouter as Router,
   Switch,
--- src/components/LaunchView/LaunchView.jsx
+++ src/components/LaunchView/LaunchView.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import axios from 'axios';
+import { withRouter } from 'react-router';
+import './styles.css'
+
 export class LaunchView extends React.Component {
-  render() {
+
+  state = {
+    launch: {}
+  }
+
+  componentDidMount = () => {
+    const flightNo = this.props.match.params.flight_number
+    this.getLaunch(flightNo)
+  }
+
+  getLaunch = (flightNo) => {
+    axios.get('https://api.spacexdata.com/v3/launches/' + flightNo).then((response) => {
+      this.setState(
+        { launch: response.data }
+      )
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  launchAttribute = (title, key) => {
+    const value = this.state.launch[key]
     return (
-      <div>
-        Launches View
+      <div className="attribute">
+        <h1>{title}</h1>
+        <h1>{value}</h1>
+      </div>
+    )
+  }
+
+  render() {
+    console.log(this.state.launch)
+    return (
+      <div className="launch-view">
+        {this.launchAttribute("Mission Name", "mission_name")}
+        {this.launchAttribute("Flight Number", "flight_number")}
+        {this.launchAttribute("Launch Date", "launch_date_local")}
+        <p className='launch-description'>{this.state.launch.details}</p>
       </div>
     )
   }
 }
+
+export default withRouter(LaunchView);
